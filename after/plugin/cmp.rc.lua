d5e6f6bde518e39f8aa505efe62face72714b7ab
guxuerui <im.guxuerui@gmail.com>
2022-11-05 17:19:06 +0800

    feat: add code snippets

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -5,24 +5,57 @@ local lspkind = require 'lspkind'
 cmp.setup({
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+       local luasnip = prequire "luasnip"
+       if not luasnip then
+           return
+       end
+       luasnip.lsp_expand(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
     }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      local luasnip = require "luasnip"
+      if cmp.visible() then
+          cmp.select_next_item()
+      elseif luasnip.expandable() then
+          luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+      elseif check_backspace() then
+          fallback()
+      else
+          fallback()
+      end
+     end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      local luasnip = require "luasnip"
+      if cmp.visible() then
+          cmp.select_next_item()
+      elseif luasnip.expandable() then
+          luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+      elseif check_backspace() then
+          fallback()
+      else
+          fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
+    { name = "luasnip", priority = 1 },
+    { name = 'nvim_lsp', priority = 2 },
+    { name = 'buffer', priority = 3 },
   }),
   formatting = {
     format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,6 +25,13 @@ packer.startup(function(use)
   use 'neovim/nvim-lspconfig'
   use 'onsails/lspkind-nvim'
   use 'L3MON4D3/LuaSnip'
+  use {
+    "L3MON4D3/LuaSnip",
+    after = "nvim-cmp",
+    config = function()
+        require "after.plugin.snippets.rc"
+    end,
+  }
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/nvim-cmp'
