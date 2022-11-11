74d7895842aa5da4554f27922298a429386cbd2a
guxuerui <im.guxuerui@gmail.com>
2022-11-11 18:58:02 +0800

    feat(whichkey.rc.lua): config and import which-key.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -71,16 +71,7 @@ packer.startup(function(use)
         "MunifTanjim/nui.nvim",
       }
    }
-  use {
-      "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-      end
-   }
+  use "folke/which-key.nvim"
   use "easymotion/vim-easymotion"
   use {
       'numToStr/Comment.nvim',
