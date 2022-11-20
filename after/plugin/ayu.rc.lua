8f1b91c400b3ba5808a42f5693113b7479386eaf
guxuerui <im.guxuerui@gmail.com>
2022-11-20 18:02:13 +0800

    fix(ayu.rc.lua): 修改ayu配置文件格式

--- after/plugin/ayu.rc.lua
+++ after/plugin/ayu.rc.lua
@@ -1,6 +1,6 @@
 require('ayu').setup({
-    mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-    overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+  mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+  overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
 })
 require('ayu').colorscheme()
 
