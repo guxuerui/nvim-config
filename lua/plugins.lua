3e8340a03403e8349ca395a1f9dac7d83e213484
guxuerui <im.guxuerui@gmail.com>
2022-11-23 14:07:00 +0800

    refactor(plugin.lua): update code style

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,13 +64,13 @@ packer.startup(function(use)
   use "akinsho/toggleterm.nvim"
   use {
     "nvim-neo-tree/neo-tree.nvim",
-      branch = "v2.x",
-      requires = { 
-        "nvim-lua/plenary.nvim",
-        "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-      }
-   }
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
   use "folke/which-key.nvim"
   use "easymotion/vim-easymotion"
   use {
