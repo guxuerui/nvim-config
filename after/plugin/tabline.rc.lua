2c1fcc5cf7b34c274c435930fbafc8f70f679567
guxuerui <im.guxuerui@gmail.com>
2022-11-29 20:13:37 +0800

    fix(tabline.rc.lua): change max_bufferline_percent value from 66 to 99

--- after/plugin/tabline.rc.lua
+++ after/plugin/tabline.rc.lua
@@ -11,7 +11,7 @@ require('tabline').setup {
     -- These options can be used to override those settings.
     section_separators = {'', ''},
     component_separators = {'', ''},
-    max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+    max_bufferline_percent = 99, -- set to nil by default, and it uses vim.o.columns * 2/3
     show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
     show_devicons = true, -- this shows devicons in buffer section
     show_bufnr = false, -- this appends [bufnr] to buffer section,
