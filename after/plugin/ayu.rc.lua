58498f569a5aed6232f859610f40e2a162c93de8
guxuerui <im.guxuerui@gmail.com>
2022-12-15 11:05:01 +0800

    delete(ayu.rc.lua): delete useless comment

--- after/plugin/ayu.rc.lua
+++ after/plugin/ayu.rc.lua
@@ -3,6 +3,3 @@ require('ayu').setup({
   overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
 })
 require('ayu').colorscheme()
-
--- Or use other theme -> oh-lucy.nvim
--- vim.cmd[[colorscheme oh-lucy-evening]]
