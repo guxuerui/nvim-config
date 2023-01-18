133a7faa3a855d5fe08020f5bb994add205e7313
guxuerui <im.guxuerui@gmail.com>
2023-01-18 20:53:42 +0800

    fix(lspsaga.rc.lua): update plugin config

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -4,8 +4,39 @@ if not modes_status_ok then
 	return
 end
 
-lspsaga.init_lsp_saga({
+lspsaga.setup({
 	-- your configuration
+  ui = {
+    -- currently only round theme
+    theme = 'round',
+    -- border type can be single,double,rounded,solid,shadow.
+    border = 'solid',
+    winblend = 0,
+    expand = '',
+    collapse = '',
+    preview = ' ',
+    code_action = '💡',
+    diagnostic = '🐞',
+    incoming = ' ',
+    outgoing = ' ',
+    colors = {
+      --float window normal bakcground color
+      normal_bg = '#1d1536',
+      --title background color
+      title_bg = '#afd700',
+      red = '#e95678',
+      magenta = '#b33076',
+      orange = '#FF8700',
+      yellow = '#f7bb3b',
+      green = '#afd700',
+      cyan = '#36d0e0',
+      blue = '#61afef',
+      purple = '#CBA6F7',
+      white = '#d1d4cf',
+      black = '#1c1c19',
+    },
+    kind = {},
+  },
 })
 
 -- Shorten function name
