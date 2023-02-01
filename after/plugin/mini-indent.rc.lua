17dfe4bd02ce6f4f0f9fc82c1fb80620aff77ddd
guxuerui <im.guxuerui@gmail.com>
2023-02-01 10:32:14 +0800

    feat(mini): update plugin config

--- after/plugin/mini-indent.rc.lua
+++ after/plugin/mini-indent.rc.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+  callback = function()
+    vim.b.miniindentscope_disable = true
+  end,
+})
+
 require('mini.indentscope').setup {
   symbol = "│",
   options = { try_as_border = true },
