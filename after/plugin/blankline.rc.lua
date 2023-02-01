9fbb003f1421f3847e45a77340bf091149428e4a
guxuerui <im.guxuerui@gmail.com>
2023-02-01 10:26:16 +0800

    fix(plugin): update plugin indent_blankline config

--- after/plugin/blankline.rc.lua
+++ after/plugin/blankline.rc.lua
@@ -11,16 +11,21 @@ vim.opt.list = true
 vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
-  space_char_blankline = " ",
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-    "IndentBlanklineIndent3",
-    "IndentBlanklineIndent4",
-    "IndentBlanklineIndent5",
-    "IndentBlanklineIndent6",
-  },
-  show_current_context = true,
-  show_current_context_start = true,
-  show_end_of_line = true,
+  -- space_char_blankline = " ",
+  -- char_highlight_list = {
+  --   "IndentBlanklineIndent1",
+  --   "IndentBlanklineIndent2",
+  --   "IndentBlanklineIndent3",
+  --   "IndentBlanklineIndent4",
+  --   "IndentBlanklineIndent5",
+  --   "IndentBlanklineIndent6",
+  -- },
+  -- show_current_context = true,
+  -- show_current_context_start = true,
+  -- show_end_of_line = true,
+  -- char = "▏",
+  char = "│",
+  filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+  show_trailing_blankline_indent = false,
+  show_current_context = false,
 }
