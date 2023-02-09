0a186dab8d33fed42f8a40893525fd1b53171ed2
guxuerui <im.guxuerui@gmail.com>
2023-02-09 14:49:49 +0800

    refactor(colorscheme): update code style

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  { 
+  {
     'folke/tokyonight.nvim',
     name = "tokyonight",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme tokyonight-moon]])
-		end,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight-moon]])
+    end,
   },
 }
