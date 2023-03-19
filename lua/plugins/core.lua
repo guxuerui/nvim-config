e8a7cce73872eaa6b91af94e6ad01720b6b39cd6
guxuerui <im.guxuerui@gmail.com>
2023-03-19 22:10:50 +0800

    feat(plugins): install plugin codeium.vim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -132,5 +132,15 @@ return {
       width = 200,
     }
   },
-  "fedepujol/move.nvim"
+  "fedepujol/move.nvim",
+  {
+    'Exafunction/codeium.vim',
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-3) end, { expr = true })
+      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end
+  }
 }
