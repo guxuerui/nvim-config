fb504618fc24dc84621e10b3647cb50be6a58eef
guxuerui <im.guxuerui@gmail.com>
2023-04-18 13:54:19 +0800

    🎉 feat(shade.lua): add shade.nvim plugin and configure it with overlay opacity, opacity step, and keybindings

---
+++ lua/plugins/shade.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "sunjon/shade.nvim",
+    config = function()
+      require("shade").setup({
+        overlay_opacity = 50,
+        opacity_step = 1,
+        keys = {
+          brightness_up   = '<C-Up>',
+          brightness_down = '<C-Down>',
+          toggle          = '<C-s>',
+        }
+      })
+    end
+  }
+}
