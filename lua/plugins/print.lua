326735d9a24faca5ed673c6acd822c519e23c157
guxuerui <im.guxuerui@gmail.com>
2023-05-04 14:40:00 +0800

    🔨 refactor(print.lua): remove unnecessary comments and newline at end of file

--- lua/plugins/print.lua
+++ lua/plugins/print.lua
@@ -2,16 +2,9 @@ return {
   {
     'rareitems/printer.nvim',
     opts = {
-      keymap = "gp",-- Plugin doesn't have any keymaps by default
-      behavior = "insert_below", -- how operator should behave
-      -- "insert_below" will insert the text below the cursor
-      --  "yank" will not insert but instead put text into the default '"' register
+      keymap = "gp",
+      behavior = "insert_below",
       formatters = {
-        -- you can define your formatters for specific filetypes
-        -- by assigning function that takes two strings
-        -- one text modified by 'add_to_inside' function
-        -- second the variable (thing) you want to print out
-        -- see examples in lua/formatters.lua
         lua = function(inside, variable)
           return string.format('print("%s: " .. %s)', inside, variable)
         end,
@@ -30,8 +23,9 @@ return {
       },
       -- function which modifies the text inside string in the print statement, by default it adds the path and line number
       add_to_inside = function(text)
-          return string.format("[%s:%s] %s", vim.fn.expand("%"), vim.fn.line("."), text)
+        return string.format("[%s:%s] %s", vim.fn.expand("%"), vim.fn.line("."), text)
       end
     }
   }
 }
+
