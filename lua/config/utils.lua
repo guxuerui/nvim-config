1a9e501840a3b10bd5764538a3b3f3a435897b90
guxuerui <im.guxuerui@gmail.com>
2023-05-05 16:39:23 +0800

    feat(utils.lua): add quotes when type equal exclude svelte components

---
+++ lua/config/utils.lua
@@ -0,0 +1,16 @@
+-- Inside an attribute: <button type| pressing = -> <button type="|".
+-- Not applicable when passing properties in `Svelte` components.
+vim.keymap.set('i', '=', function()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
+
+  -- The cursor location does not give us the correct node in this case, so we
+  -- need to get the node to the left of the cursor
+  local node = vim.treesitter.get_node { pos = left_of_cursor_range }
+  local nodes_active_in = { 'attribute_name', 'directive_argument', 'directive_name' }
+  if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
+    return '='
+  end
+
+  return '=""<left>'
+end, { expr = true, buffer = true })
