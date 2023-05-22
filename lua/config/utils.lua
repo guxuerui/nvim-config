697024e4c707090f641a09212ea0044a21fc8a2b
guxuerui <im.guxuerui@gmail.com>
2023-05-22 10:44:43 +0800

    📝 docs(utils.lua): add comment explaining the strange behavior of the code

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -4,8 +4,7 @@ vim.keymap.set('i', '=', function()
   local cursor = vim.api.nvim_win_get_cursor(0)
   local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
 
-  -- The cursor location does not give us the correct node in this case, so we
-  -- need to get the node to the left of the cursor
+  -- Sometimes it doesn't work, a bit strange....
   local node = vim.treesitter.get_node { pos = left_of_cursor_range }
   local nodes_active_in = { 'attribute_name', 'directive_argument', 'directive_name' }
   if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
