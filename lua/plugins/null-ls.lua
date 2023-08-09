696c3a05a6becd248127a020b93d64c9381fa9b2
guxuerui <im.guxuerui@gmail.com>
2023-08-09 19:47:04 +0800

    chore(null-ls.lua): update null-ls configuration
    - Remove commented out code for formatting function
    - Add support for formatting with stylua
    - Set formatexpr option for web dev files
    - Filter null-ls for formatting instead of lsp server
    - Update eslint_d configuration with extra arguments

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,42 +1,53 @@
 return {
-  'jose-elias-alvarez/null-ls.nvim',
-  config = function()
-    local null_ls = require("null-ls")
-    local formatting = null_ls.builtins.formatting
-    -- local diagnostics = null_ls.builtins.diagnostics
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"jose-elias-alvarez/null-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting
+		-- local diagnostics = null_ls.builtins.diagnostics
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    local format = function()
-      vim.lsp.buf.format({
-        async = false,
-      })
-    end
+		-- local format = function()
+		-- 	vim.lsp.buf.format({
+		-- 		async = false,
+		-- 	})
+		-- end
 
-    null_ls.setup({
-      debug = false,
-      sources = {
-        formatting.markdownlint, -- need install: npm install -g markdownlint-cli
-        formatting.eslint_d,
-        -- diagnostics.eslint_d,
-      },
-      -- you can reuse a shared lspconfig on_attach callback here
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = format,
-          })
-        end
-      end,
-    })
+		null_ls.setup({
+			debug = false,
+			sources = {
+				formatting.markdownlint, -- need install: npm install -g markdownlint-cli
+				formatting.stylua,
+				formatting.eslint_d,
+				-- diagnostics.eslint_d,
+			},
+			-- you can reuse a shared lspconfig on_attach callback here
+			on_attach = function(current_client, bufnr)
+				if current_client.supports_method("textDocument/formatting") then
+					-- format web dev files
+					vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.buf.format()")
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({
+								filter = function(client)
+									--  only use null-ls for formatting instead of lsp server
+									return client.name == "null-ls"
+								end,
+								bufnr = bufnr,
+							})
+						end,
+					})
+				end
+			end,
+		})
 
-    null_ls.builtins.formatting.eslint_d.with({
-      extra_args = {
-        "--style",
-        "{IndentWidth: 2 ,ColumnLimit: 120}",
-      },
-    })
-  end
+		null_ls.builtins.formatting.eslint_d.with({
+			extra_args = {
+				"--style",
+				"{IndentWidth: 2 ,ColumnLimit: 120}",
+			},
+		})
+	end,
 }
