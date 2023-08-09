15ffcc78e33e9442f235cc65ee72c0bcc73c899d
guxuerui <im.guxuerui@gmail.com>
2023-08-09 19:45:38 +0800

    refactor: format code

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,30 +1,30 @@
 return {
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    dependencies = {
-      "lukas-reineke/lsp-format.nvim",
-      "neovim/nvim-lspconfig",
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
-      "onsails/lspkind-nvim",
-    },
-    config = function()
-      local lsp = require("lsp-zero")
-      lsp.preset("recommended")
-      -- lsp.on_attach(function(client, bufnr)
-      --   require("lsp-format").on_attach(client, bufnr)
-      -- end)
-      lsp.nvim_workspace()
-      lsp.setup()
-      vim.diagnostic.config({ virtual_text = true })
-    end,
-  }
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		dependencies = {
+			"lukas-reineke/lsp-format.nvim",
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind-nvim",
+		},
+		config = function()
+			local lsp = require("lsp-zero")
+			lsp.preset("recommended")
+			-- lsp.on_attach(function(client, bufnr)
+			--   require("lsp-format").on_attach(client, bufnr)
+			-- end)
+			lsp.nvim_workspace()
+			lsp.setup()
+			vim.diagnostic.config({ virtual_text = true })
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,56 +1,56 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = "BufReadPost",
-    config = function()
-      local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
-      ft_to_parser.mdx = "markdown"
-      require("nvim-treesitter.configs").setup({
-        -- one of "all"
-        ensure_installed = {
-          "javascript",
-          "css",
-          "scss",
-          "typescript",
-          "tsx",
-          "json",
-          "vue",
-          "python",
-          "html",
-          "lua",
-          "svelte",
-          "vim",
-          "help",
-          "astro",
-          "markdown",
-          "markdown_inline",
-          "bash",
-          "fish",
-          "prisma",
-        },
-        sync_install = true,
-        ignore_install = { "php", "phpdoc" },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = true,
-        },
-        indent = {
-          enable = true,
-        },
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-        },
-        rainbow = {
-          enable = true,
-          -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-          extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-          max_file_lines = nil, -- Do not enable for files with more than n lines, int
-          -- colors = {}, -- table of hex strings
-          -- termcolors = {} -- table of colour name strings
-        },
-      })
-    end,
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = "BufReadPost",
+		config = function()
+			local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
+			ft_to_parser.mdx = "markdown"
+			require("nvim-treesitter.configs").setup({
+				-- one of "all"
+				ensure_installed = {
+					"javascript",
+					"css",
+					"scss",
+					"typescript",
+					"tsx",
+					"json",
+					"vue",
+					"python",
+					"html",
+					"lua",
+					"svelte",
+					"vim",
+					"help",
+					"astro",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"fish",
+					"prisma",
+				},
+				sync_install = true,
+				ignore_install = { "php", "phpdoc" },
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = true,
+				},
+				indent = {
+					enable = true,
+				},
+				context_commentstring = {
+					enable = true,
+					enable_autocmd = false,
+				},
+				rainbow = {
+					enable = true,
+					-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+					extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+					max_file_lines = nil, -- Do not enable for files with more than n lines, int
+					-- colors = {}, -- table of hex strings
+					-- termcolors = {} -- table of colour name strings
+				},
+			})
+		end,
+	},
 }
