46d76b0696b4cd71c430fdb4aebb76365249ba1a
guxuerui <im.guxuerui@gmail.com>
2023-08-10 09:52:57 +0800

    fix(utils.lua): fix indentation and change single quotes to double quotes for consistency

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,15 +1,15 @@
 -- Inside an attribute: <button type| pressing = -> <button type="|".
 -- Not applicable when passing properties in `Svelte` components.
-vim.keymap.set('i', '=', function()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
+vim.keymap.set("i", "=", function()
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
 
-  -- Sometimes it doesn't work, a bit strange....
-  local node = vim.treesitter.get_node { pos = left_of_cursor_range }
-  local nodes_active_in = { 'attribute_name', 'directive_argument', 'directive_name' }
-  if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
-    return '='
-  end
+	-- Sometimes it doesn't work, a bit strange....
+	local node = vim.treesitter.get_node({ pos = left_of_cursor_range })
+	local nodes_active_in = { "attribute_name", "directive_argument", "directive_name" }
+	if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
+		return "="
+	end
 
-  return '=""<left>'
+	return '=""<left>'
 end, { expr = true, buffer = true })
