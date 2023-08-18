e3088772324991fa7fc228c615437c4c1348c151
guxuerui <im.guxuerui@gmail.com>
2023-08-18 13:54:49 +0800

    fix(mini.lua): fix indentation and formatting issues feat(mini.lua): add support for mini.nvim plugin with configuration options

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,40 +1,39 @@
 -- Setting specific file types does not work
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-  callback = function()
-    vim.b.miniindentscope_disable = true
-  end,
+	pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+	callback = function()
+		vim.b.miniindentscope_disable = true
+	end,
 })
 
 return {
-  {
-    'echasnovski/mini.nvim',
-    version = false,
-    config = function()
-      require('mini.indentscope').setup {
-        symbol = "│",
-        options = { try_as_border = true },
-      }
-      require('mini.cursorword').setup()
-      require('mini.bracketed').setup()
-      require('mini.jump').setup()
-      require('mini.animate').setup()
-      require('mini.files').setup(
-        {
-          options = {
-            use_as_default_explorer = true,
-          },
-          windows = {
-            -- Maximum number of windows to show side by side
-            max_number = math.huge,
-            -- Whether to show preview of directory under cursor
-            preview = true,
-            -- Width of focused window
-            width_focus = 50,
-            -- Width of non-focused window
-            width_nofocus = 25,
-          },
-        })
-    end
-  }
+	{
+		"echasnovski/mini.nvim",
+		version = false,
+		config = function()
+			require("mini.indentscope").setup({
+				symbol = "│",
+				options = { try_as_border = true },
+			})
+			require("mini.cursorword").setup()
+			require("mini.bracketed").setup()
+			require("mini.jump").setup()
+			require("mini.animate").setup()
+			require("mini.files").setup({
+				options = {
+					use_as_default_explorer = true,
+				},
+				windows = {
+					-- Maximum number of windows to show side by side
+					max_number = math.huge,
+					-- Whether to show preview of directory under cursor
+					preview = true,
+					-- Width of focused window
+					width_focus = 50,
+					-- Width of non-focused window
+					width_nofocus = 25,
+				},
+			})
+		end,
+	},
 }
