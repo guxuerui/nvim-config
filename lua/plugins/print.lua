369ecbb24ee2857ec96885dcaff0ba590909e1f2
guxuerui <im.guxuerui@gmail.com>
2023-09-05 14:28:41 +0800

    chore(print.lua): reformat code for better readability and consistency

--- lua/plugins/print.lua
+++ lua/plugins/print.lua
@@ -1,31 +1,33 @@
 return {
-  {
-    'rareitems/printer.nvim',
-    opts = {
-      keymap = "gp",
-      behavior = "insert_below",
-      formatters = {
-        lua = function(inside, variable)
-          return string.format('print("%s: " .. %s)', inside, variable)
-        end,
-        vue = function(inside, variable)
-          return string.format("console.log('%s: ', %s)", inside, variable)
-        end,
-        ts = function(inside, variable)
-          return string.format("console.log('%s: ', %s)", inside, variable)
-        end,
-        js = function(inside, variable)
-          return string.format("console.log('%s: ', %s)", inside, variable)
-        end,
-        svelte = function(inside, variable)
-          return string.format("console.log('%s: ', %s)", inside, variable)
-        end,
-      },
-      -- function which modifies the text inside string in the print statement, by default it adds the path and line number
-      add_to_inside = function(text)
-        return string.format("[%s:%s] %s", vim.fn.expand("%"), vim.fn.line("."), text)
-      end
-    }
-  }
+	{
+		"rareitems/printer.nvim",
+		opts = {
+			keymap = "gp",
+			behavior = "insert_below",
+			formatters = {
+				lua = function(inside, variable)
+					return string.format('print("%s: " .. %s)', inside, variable)
+				end,
+				vue = function(inside, variable)
+					return string.format("console.log('%s: ', %s)", inside, variable)
+				end,
+				ts = function(inside, variable)
+					return string.format("console.log('%s: ', %s)", inside, variable)
+				end,
+				html = function(inside, variable)
+					return string.format("console.log('%s: ', %s)", inside, variable)
+				end,
+				js = function(inside, variable)
+					return string.format("console.log('%s: ', %s)", inside, variable)
+				end,
+				svelte = function(inside, variable)
+					return string.format("console.log('%s: ', %s)", inside, variable)
+				end,
+			},
+			-- function which modifies the text inside string in the print statement, by default it adds the path and line number
+			add_to_inside = function(text)
+				return string.format("[%s:%s] %s", vim.fn.expand("%"), vim.fn.line("."), text)
+			end,
+		},
+	},
 }
-
