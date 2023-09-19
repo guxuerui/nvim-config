b5dbc53fa16e242662dfecbd25780032d49e96ea
guxuerui <im.guxuerui@gmail.com>
2023-09-19 15:03:05 +0800

    fix(alpha.lua): fix indentation and formatting issues feat(alpha.lua): add support for custom find_files command in dashboard button

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    "goolord/alpha-nvim",
-    lazy = false,
-    event = "VimEnter",
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
-      local logo = [[
+	{
+		"goolord/alpha-nvim",
+		lazy = false,
+		event = "VimEnter",
+		opts = function()
+			local dashboard = require("alpha.themes.dashboard")
+			local logo = [[
      ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
      ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
      ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
@@ -14,83 +14,92 @@ return {
      ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
       ]]
 
-      local function getGreeting(name)
-        local tableTime = os.date("*t")
-        local datetime = os.date(" %Y-%m-%d   %H:%M:%S")
-        local hour = tableTime.hour
-        local greetingsTable = {
-          [1] = "  Sleep well",
-          [2] = "  Good morning",
-          [3] = "  Good afternoon",
-          [4] = "  Good evening",
-          [5] = "󰖔  Good night",
-        }
-        local greetingIndex = 0
-        if hour == 23 or hour < 7 then
-          greetingIndex = 1
-        elseif hour < 12 then
-          greetingIndex = 2
-        elseif hour >= 12 and hour < 18 then
-          greetingIndex = 3
-        elseif hour >= 18 and hour < 21 then
-          greetingIndex = 4
-        elseif hour >= 21 then
-          greetingIndex = 5
-        end
-        return "\t   " .. datetime .. "\t" .. greetingsTable[greetingIndex] .. ", " .. name
-      end
+			local function getGreeting(name)
+				local tableTime = os.date("*t")
+				local datetime = os.date(" %Y-%m-%d   %H:%M:%S")
+				local hour = tableTime.hour
+				local greetingsTable = {
+					[1] = "  Sleep well",
+					[2] = "  Good morning",
+					[3] = "  Good afternoon",
+					[4] = "  Good evening",
+					[5] = "󰖔  Good night",
+				}
+				local greetingIndex = 0
+				if hour == 23 or hour < 7 then
+					greetingIndex = 1
+				elseif hour < 12 then
+					greetingIndex = 2
+				elseif hour >= 12 and hour < 18 then
+					greetingIndex = 3
+				elseif hour >= 18 and hour < 21 then
+					greetingIndex = 4
+				elseif hour >= 21 then
+					greetingIndex = 5
+				end
+				return "\t   " .. datetime .. "\t" .. greetingsTable[greetingIndex] .. ", " .. name
+			end
 
-      local userName = "Qishi"
-      local greeting = getGreeting(userName)
-      dashboard.section.header.val = vim.split(logo .. "\n" .. greeting, "\n")
-      dashboard.section.buttons.val = {
-        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("b", " " .. " File browser", ":Telescope file_browser <CR>"),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-        dashboard.button("m", "勒" .. " Key mappings", ":e ~/.config/nvim/lua/config/keymaps.lua <CR>"),
-        dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-      }
-      -- set highlight
-      for _, button in ipairs(dashboard.section.buttons.val) do
-        button.opts.hl = "AlphaButtons"
-        button.opts.hl_shortcut = "AlphaShortcut"
-      end
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButtons"
-      dashboard.section.footer.opts.hl = "AlphaFooter"
-      dashboard.opts.layout[1].val = 8
-      return dashboard
-    end,
-    config = function(_, dashboard)
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "AlphaReady",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
+			local userName = "Qishi"
+			local greeting = getGreeting(userName)
+			dashboard.section.header.val = vim.split(logo .. "\n" .. greeting, "\n")
+			dashboard.section.buttons.val = {
+				dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
+				dashboard.button(
+					"f",
+					" " .. " Find file",
+					":lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }}) <CR>"
+				),
+				dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
+				dashboard.button("b", " " .. " File browser", ":Telescope file_browser <CR>"),
+				dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+				dashboard.button("m", "勒" .. " Key mappings", ":e ~/.config/nvim/lua/config/keymaps.lua <CR>"),
+				dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+				dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+			}
+			-- set highlight
+			for _, button in ipairs(dashboard.section.buttons.val) do
+				button.opts.hl = "AlphaButtons"
+				button.opts.hl_shortcut = "AlphaShortcut"
+			end
+			dashboard.section.header.opts.hl = "AlphaHeader"
+			dashboard.section.buttons.opts.hl = "AlphaButtons"
+			dashboard.section.footer.opts.hl = "AlphaFooter"
+			dashboard.opts.layout[1].val = 8
+			return dashboard
+		end,
+		config = function(_, dashboard)
+			-- close Lazy and re-open when the dashboard is ready
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "AlphaReady",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
 
-      require("alpha").setup(dashboard.opts)
+			require("alpha").setup(dashboard.opts)
 
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyVimStarted",
-        callback = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          local version = "  󰥱 v" .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
-          local plugins = "⚡Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-          local footer = version .. "\t" .. plugins .. "\n"
-          dashboard.section.footer.val = footer
-          pcall(vim.cmd.AlphaRedraw)
-        end,
-      })
-    end,
-  },
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LazyVimStarted",
+				callback = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					local version = "  󰥱 v"
+						.. vim.version().major
+						.. "."
+						.. vim.version().minor
+						.. "."
+						.. vim.version().patch
+					local plugins = "⚡Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+					local footer = version .. "\t" .. plugins .. "\n"
+					dashboard.section.footer.val = footer
+					pcall(vim.cmd.AlphaRedraw)
+				end,
+			})
+		end,
+	},
 }
