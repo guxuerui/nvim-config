66b5ead04d7ab1f001efdfc537fc33894f6d3558
guxuerui <im.guxuerui@gmail.com>
2023-09-19 11:05:25 +0800

    chore(lsp.lua): comment out unused lsp-format.nvim dependency feat(lsp.lua): add LSP support dependencies feat(lsp.lua): add autocompletion dependencies

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,10 +2,12 @@ return {
 	{
 		"VonHeikemen/lsp-zero.nvim",
 		dependencies = {
-			"lukas-reineke/lsp-format.nvim",
+			-- "lukas-reineke/lsp-format.nvim",
+			-- LSP Support
 			"neovim/nvim-lspconfig",
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
+			-- Autocompletion
 			"hrsh7th/nvim-cmp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
