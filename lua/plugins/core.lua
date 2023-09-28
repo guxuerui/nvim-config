a915b36cdac0bb9c930bf098eb5012967ebedcd9
guxuerui <im.guxuerui@gmail.com>
2023-09-28 16:45:14 +0800

    chore(core.lua): remove unnecessary comments and update treesitter configuration

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -81,13 +81,9 @@ return {
 		"Wansmer/treesj",
 		opts = {
 			use_default_keymaps = false,
-			-- Node with syntax error will not be formatted
 			check_syntax_error = true,
-			-- If line after join will be longer than max value,
-			-- node will not be formatted
 			max_join_length = 120,
 			cursor_behavior = "hold",
-			-- Notify about possible problems or not
 			notify = true,
 		},
 	},
