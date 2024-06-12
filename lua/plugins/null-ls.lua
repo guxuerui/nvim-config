99315f8178d4c82dcccb2980f8036186168c9f4e
guxuerui <im.guxuerui@gmail.com>
2024-06-12 17:01:05 +0800

    chore(null-ls.lua): refactor null-ls configuration to include additional sources feat(null-ls.lua): add support for null-ls formatting and completion sources feat(null-ls.lua): implement timeout for null-ls setup to improve performance

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,6 +1,14 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+		"gbprod/none-ls-shellcheck.nvim",
+	},
+	event = "BufReadPre",
 	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 		local lsp_formatting = function(bufnr)
 			vim.lsp.buf.format({
 				filter = function(client)
@@ -10,17 +18,17 @@ return {
 				bufnr = bufnr,
 			})
 		end
-		local null_ls = require("null-ls")
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		local formatting = null_ls.builtins.formatting
-		local completion = null_ls.builtins.completion
-
 		null_ls.setup({
-			debug = false,
+			timeout_ms = 5000,
 			sources = {
-				formatting.markdownlint,
-				formatting.stylua,
-				completion.spell,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.completion.spell,
+				null_ls.builtins.formatting.shfmt,
+				require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.formatting.eslint_d"),
+				require("none-ls.code_actions.eslint_d"),
+				require("none-ls-shellcheck.diagnostics"),
+				require("none-ls-shellcheck.code_actions"),
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
@@ -35,42 +43,5 @@ return {
 				end
 			end,
 		})
-
-		-- null_ls.setup({
-		-- 	debug = false,
-		-- 	sources = {
-		-- 		formatting.markdownlint,
-		-- 		formatting.stylua,
-		-- 		formatting.eslint_d,
-		-- 	},
-		-- 	-- you can reuse a shared lspconfig on_attach callback here
-		-- 	on_attach = function(current_client, bufnr)
-		-- 		if current_client.supports_method("textDocument/formatting") then
-		-- 			-- format web dev files
-		-- 			vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.buf.format()")
-		-- 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		-- 			vim.api.nvim_create_autocmd("BufWritePre", {
-		-- 				group = augroup,
-		-- 				buffer = bufnr,
-		-- 				callback = function()
-		-- 					vim.lsp.buf.format({
-		-- 						filter = function(client)
-		-- 							--  only use null-ls for formatting instead of lsp server
-		-- 							return client.name == "null-ls"
-		-- 						end,
-		-- 						bufnr = bufnr,
-		-- 					})
-		-- 				end,
-		-- 			})
-		-- 		end
-		-- 	end,
-		-- })
-
-		-- null_ls.builtins.formatting.eslint_d.with({
-		-- 	extra_args = {
-		-- 		"--style",
-		-- 		"{IndentWidth: 2 ,ColumnLimit: 120}",
-		-- 	},
-		-- })
 	end,
 }
