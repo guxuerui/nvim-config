a31270076554f6c25acd6c5b124b3d1e2ade3e54
guxuerui <im.guxuerui@gmail.com>
2024-06-13 14:06:59 +0800

    refactor(null-ls.lua): remove commented out eslint_d diagnostics and code actions to clean up the file fix(null-ls.lua): fix formatting of markdownlint import to ensure consistency

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -25,7 +25,7 @@ return {
 				null_ls.builtins.formatting.markdownlint,
 				-- require("none-ls.diagnostics.eslint_d"),
 				require("none-ls.formatting.eslint_d"),
-				require("none-ls.code_actions.eslint_d"),
+				-- require("none-ls.code_actions.eslint_d"),
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
